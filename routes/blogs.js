c9b2af4a687541cc978003a6e723dc3002676122
Xi Zhu <zhuxi28@hotmail.com>
2015-11-29 19:57:30 -0500

    Blog Working!!

--- routes/blogs.js
+++ routes/blogs.js
@@ -1,26 +1,45 @@
-var Blog = require('../lib/blog/post.js');
+var Post = require('../lib/blog/post.js');
 
 exports.list = function(req, res, next) {
     var page = req.page;
     var type = req.params.type || 'overview';
-    var overview = req.params.perpage==null?false:true;
-    Blog.getRange(page.number, page.perpage, type, function(err, posts){
+    var fullpage = req.params.fullpage;  // if params.fullpage not defined (fullpage null) => fullpage
+    Post.getByRange(page.number, page.perpage, type, function(err, posts){
     	if (err) return next(err);
-    	res.render('public/posts', {
-    		posts: posts,
-    		page: page.number,
-            reqpage: !overview,
-    		pages: page.count
-    	});
-    	//res.end(JSON.stringify(posts));
+        if (!fullpage) {
+            res.render('public/fullpage', {
+                posts: posts,
+                type: type,
+                page: page.number,
+                pages: page.count
+            });
+        } else {
+            res.render('public/section', {
+                posts: posts
+            });
+        }
+    	
     })
 };
 
 exports.submit = function(req, res, next) {
     var data = req.body;
-    Blog.save(data, function(err) {
+    Post.save(data, function(err) {
         if (err) return next(err);
     });
     res.redirect('/blog/' + data.category);
 }
 
+exports.post = function(req, res, next) {
+    var id = req.params.id;
+    Post.getById(id, function(err, post) {
+        if (err) return next(err);
+        res.render('public/post',
+        {
+            title: post.post_title,
+            date: post.post_date,
+            content: post.post,
+            //type: post.type
+        });
+    })
+}
